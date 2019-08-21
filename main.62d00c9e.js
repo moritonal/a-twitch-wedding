80d9397481dceb7eec031e4439c6d53af0b895fe
Tom Bonner <tom+github@bonner.is>
2019-08-21 09:24:44 +0100

    Updates

--- main.62d00c9e.js
+++ main.62d00c9e.js
@@ -38326,7 +38326,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6156" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "23234" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
